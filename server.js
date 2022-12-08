4bc8f715f12c5c6d834963be9d4329d07cb579d2
Andrew Toussaint <ajntoussaint@gmail.com>
2022-12-08 07:36:39 -0500

    feat: added express routing and test html page. Began work on scss implementation

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/public', express.static(process.cwd + '/public'));
+app.route('/')
+  .get(function (req,res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.listen(3000, () =>{
+  console.log("App is listening on port 3000");
+});
