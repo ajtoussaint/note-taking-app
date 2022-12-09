724d04266431c940b21e25b8b79dcf5d39656eb2
Andrew Toussaint <ajntoussaint@gmail.com>
2022-12-09 09:24:16 -0500

    feat: added register form and /login test route

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express');
+//for parsing req.body into json
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.route('/')
   .get(function (req,res) {
     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.route("/login")
+  .post(function (req,res) {
+    console.log("LOGIN ATTEMPT BY: ", req.body);
+    res.redirect("/profile");
+  });
+
+app.route("/profile")
+  .get(function (req,res) {
+    res.sendFile(process.cwd() + '/views/profile.html');
   });
 
 app.listen(3000, () =>{
