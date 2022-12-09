b4ed4c17fc5180e0d5a1193e7d514b6d21de8188
Andrew Toussaint <ajntoussaint@gmail.com>
2022-12-09 10:19:38 -0500

    feat: updated html pages to work in pug

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 const express = require('express');
 //for parsing req.body into json
 const bodyParser = require('body-parser');
+//view engine for inputing variables into html
+const pug = require('pug');
+
 
 const app = express();
 
+//bodyparser middleware setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//pug view engine setup
+app.set('view engine', 'pug');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.route('/')
   .get(function (req,res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+    //res.sendFile(process.cwd() + '/views/index.html');
+    res.render("pug/index");
   });
 
 app.route("/login")
   .post(function (req,res) {
     console.log("LOGIN ATTEMPT BY: ", req.body);
-    res.redirect("/profile");
+    res.render("pug/profile",{user:req.body.username});
+    //res.redirect("/profile");
   });
 
 app.route("/profile")
   .get(function (req,res) {
-    res.sendFile(process.cwd() + '/views/profile.html');
+    console.log("IN PROF", req.body);
+    res.render("pug/profile");
   });
 
 app.listen(3000, () =>{
